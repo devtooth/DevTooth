8e17450fe0082cb4659b0b24232639e6c9eeddae
Jimi Björkbacka <t6bjji00@students.oamk.fi>
2018-03-22 12:28:39 +0200

    Removed string

--- core/src/com/mygdx/game/OptionsScreen.java
+++ core/src/com/mygdx/game/OptionsScreen.java
@@ -44,7 +44,6 @@ public class OptionsScreen implements Screen {
     BitmapFont fps;
     int fpsInt = 0;
     String hintname = "Your Name";
-    String koira ="voihan koira";
 
     private Stage stage;
     public int row_height;
