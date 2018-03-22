cb5db1400bd991eb8d48755a0e79ea0106f73601
Jimi Björkbacka <t6bjji00@students.oamk.fi>
2018-03-22 13:34:29 +0200

    Added jimbofiles

--- core/src/com/mygdx/game/RottenRoots.java
+++ core/src/com/mygdx/game/RottenRoots.java
@@ -8,31 +8,24 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
-public class RottenRoots extends Game {
-
-
+public class RottenRoots extends Game{
 	public static final int WIDTH = 1920;
 	public static final int HEIGHT = 1080;
-	public static boolean IS_MOBILE = false;
 
 	public SpriteBatch batch;
-	//public ScrollingBackground scrollingBackground;
 	public gameCamera cam;
 	BitmapFont font;
-    private Game game;
-    Sound snap;
     public Preferences prefs;
-	RottenRoots(){
+	SoundMaster soundMaster;
+	Sound snap;
 
-    }
+	RottenRoots(){}
 
 	@Override
 	public void create () {
-        snap = Gdx.audio.newSound(Gdx.files.internal("neck_snap.mp3"));
+		soundMaster = new SoundMaster();
+
         prefs = Gdx.app.getPreferences("My Preferences");
-        //dont insert values here
-        //prefs.putBoolean("sound",true);
-        //prefs.putString("name", "Eero");
         prefs.flush();
 
 		batch = new SpriteBatch();
